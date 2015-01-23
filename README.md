2cbb4c79f1665803455847c4e66d120efdb2732d
Gabe Hamilton <gabehamilton@gmail.com>
2015-01-22 23:05:11 -0700

    Spock Sampler shows up in JMeter

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # jmeter-spock-sampler
 Run Spock specs in JMeter
+
+## To Use
+Run `mvn package` to create the jar file.
+Copy `jmeter-spock-sampler-0.1.0.jar` to `$JMETER_HOME/lib/ext`
+
+and the spock jar to `$JMETER_HOME/lib`
+
+spock jar may be found at
+`~/.m2/repository/org/spockframework/spock-core/0.7-groovy-2.0/spock-core-0.7-groovy-2.0.jar`
---
+++ src/main/java/com/github/gabehamilton/jmeter/SpockSamplerBeanInfo.java
@@ -0,0 +1,24 @@
+package com.github.gabehamilton.jmeter;
+import org.apache.jmeter.testbeans.BeanInfoSupport;
+import org.apache.jmeter.testbeans.gui.FileEditor;
+import java.beans.PropertyDescriptor;
+
+/**
+ * This class defines the bean properties that will be visually exposed for this JMeter plugin.
+ * <p/>
+ * User: Matthew McCullough
+ * Created by gabe on 1/22/15.
+ */
+public class SpockSamplerBeanInfo extends BeanInfoSupport {
+    public SpockSamplerBeanInfo() {
+        super(SpockSampler.class);
+
+        createPropertyGroup("which_spec", new String[]{"classname"});
+
+        PropertyDescriptor classname = property("classname");
+        classname.setValue(NOT_UNDEFINED, Boolean.FALSE);
+        classname.setValue(DEFAULT, "");
+        classname.setValue(NOT_EXPRESSION, Boolean.TRUE);
+        classname.setPropertyEditorClass(FileEditor.class);
+    }
+}
